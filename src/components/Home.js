ad2be84515256f149591a30212db566b8395c163
alansealy <aposengineering@gmail.com>
2021-08-20 19:35:06 -0500

    housekeeping

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,10 +8,10 @@ export default function Home() {
       <Hero />
       <div className="box cta">
         <p className="has-text-centered">
-          <span className="tag is-primary">New</span> You can customize and embed <strong>TranStat <em>Live</em></strong> Customer Web Portal directly into your website to match your existing design. Track delivery status such as ... 
+          <span className="tag is-primary">New</span> You can customize and embed <strong>TranStat <em>Live</em></strong> Web Portal directly into your website to match your existing design. Track your truck fleet status in realtime such as ... 
           <p class="col-md-2"><i><strong>Status:</strong></i>&nbsp;&nbsp;<strong>Truck:</strong> 10536 &nbsp; 
               <strong>Driver:</strong> James Franco &nbsp; <strong>Date:</strong> 08/11/2020 &nbsp; 
-              <strong>Time:</strong> 13:25:49 &nbsp; <strong>Speed:</strong> 75 kmph &nbsp; 
+              <strong>Time:</strong> 13:25:49 &nbsp; <strong>Speed:</strong> 95 kmph &nbsp; 
               <strong>Heading:</strong> SW &nbsp;  <strong>Location:</strong> I-489, New Haven, IN 46774, USA 
           </p>
         </p>
